f59b821a229bf89e9ba0e5752e593db6274a779a
danCraw <dan.craw@yandex.ru>
2020-10-10 13:01:52 +0300

    Draw algoritms

--- src/com/company/WoLineDrawer.java
+++ src/com/company/WoLineDrawer.java
@@ -5,6 +5,7 @@ import java.awt.*;
 public class WoLineDrawer implements LineDrawer {
     private PixelDrawer pd;
 
+
     WoLineDrawer(PixelDrawer pd) {
         this.pd = pd;
     }
@@ -24,38 +25,39 @@ public class WoLineDrawer implements LineDrawer {
         int dy = y2 - y1;
         //Горизонтальные и вертикальные линии не нуждаются в сглаживании
         if (dx == 0 || dy == 0) {
-            pd.drawPixel(x1, y1, java.awt.Color.BLACK);
+            pd.drawPixel(x1, y1, Color.BLACK);
             return;
         }
-        float gradient = 0;
+        float gradient = 1;
         if (dx > dy) {
             gradient = (float) dy / dx;
             float intery = y1 + gradient;
-            java.awt.Color c = (new java.awt.Color(0, 0, 0, (int) (255 - fractionalPart(intery) * 255)));
-            pd.drawPixel(x1, y1, java.awt.Color.black);
+            pd.drawPixel(x1, y1, Color.BLACK);
             for (int x = x1; x < x2; ++x) {
-//                g.setColor(new Color(0, 0, 0, (int) (fractionalPart(intery) * 255))); //Меняем прозрачность
-                pd.drawPixel(x, (int)intery, c);
+//                g.setColor(new Color(0, 0, 0, (int) (255 - fractionalPart(intery) * 255))); //Меняем прозрачность
+                pd.drawPixel(x, (int)intery, new Color(0, 0, 0, (int) (255 - fractionalPart(intery) * 255)));
 //                g.setColor(new Color(0, 0, 0, (int) (fractionalPart(intery) * 255)));
-                pd.drawPixel(x, (int)intery + 1, c);
+                pd.drawPixel(x, (int)intery + 1, new Color(0, 0, 0, (int) (fractionalPart(intery) * 255)));
                 intery += gradient;
             }
-            pd.drawPixel(x2, y2, java.awt.Color.black);
+//            g.setColor(Color.BLACK);
+            pd.drawPixel(x2, y2, Color.BLACK);
         }
         else {
             gradient = (float) dx / dy;
             float interx = x1 + gradient;
-            pd.drawPixel(x1, y1, java.awt.Color.black);
+//            g.setColor(Color.BLACK);
+            pd.drawPixel(x1, y1, Color.BLACK);
             for (int y = y1; y < y2; ++y) {
-                java.awt.Color c = (new java.awt.Color(0, 0, 0, (int) (255 - fractionalPart(interx) * 255)));
-                pd.drawPixel((int)interx, y, c);
-                java.awt.Color c2 = (new java.awt.Color(0, 0, 0, (int) (fractionalPart(interx) * 255)));
-                pd.drawPixel((int)interx + 1, y, c2);
+//                g.setColor(new Color(0, 0, 0, (int) (255 - fractionalPart(interx) * 255)));
+                pd.drawPixel((int)interx, y, new Color(0, 0, 0, (int) (255 - fractionalPart(interx) * 255)));
+//                g.setColor(new Color(0, 0, 0, (int) (fractionalPart(interx) * 255)));
+                pd.drawPixel((int)interx + 1, y, new Color(0, 0, 0, (int) (fractionalPart(interx) * 255)));
                 interx += gradient;
             }
-            pd.drawPixel(x2, y2, Color.black);
+//            g.setColor(Color.BLACK);
+            pd.drawPixel(x2, y2, Color.BLACK);
         }
-
     }
 
     private float fractionalPart(float x) {
